ae24ae2387075682fae81843f657bc06b91aff67
Guilherme Bonilha <85463234+bonilha-rogante@users.noreply.github.com>
2021-10-22 00:15:45 -0300

    Add files via upload

---
+++ nlw-heat/main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'bonilha-rogante',
+  instagram: 'bonilha.rogante'
+}
+
+/*function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}*/
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    //.then(data => alert(data.bio))
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
+
+//ARROW Function
